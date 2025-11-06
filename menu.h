db82dbd8dae1f3e7ceed6ef3db5244716c0689d0
RhennerSantos <rhennersantos2005@gmail.com>
2025-11-06 17:34:38 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -3,7 +3,14 @@
 
 //mostra o menu
 void exibir_menu();
+
 //consulta os times
 void consultar_time();
+
+//mostrar classificação
+void mostrar_classificação();
+
+//limpa a tela
+void limpa_tela();
 
 #endif
