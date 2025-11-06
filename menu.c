fd9b3da4373c92a0c6b90655446b55261209e69a
RhennerSantos <rhennersantos2005@gmail.com>
2025-11-06 17:34:13 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -3,8 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void limpa_tela()
+{
+    system("clear");
+}
 
-void exibir_menu()
+
+
+void mostrar_classificação(const int qtd_times, Times *bd_times, Partidas *bd_partidas)
+{
+    limpa_tela();
+    //TEM QUE ALINHAR ISSO AINDA
+    printf("ID\tTime\t\t\tV\tE\tD\tGM\tGS\tS\tPG\n");
+    for (int i = 0; i < qtd_times-1; i++)
+    {
+        printf("%d", bd_times[i].ID);
+        printf("\t%s", bd_times[i].nome);
+        printf("\t\t\t%d", bd_times[i].status.wins);
+        printf("\t%d", bd_times[i].status.lose);
+        printf("\t%d", bd_times[i].status.draw);
+        printf("\t%d", bd_times[i].status.w_score);
+        printf("\t%d", bd_times[i].status.l_score);
+        printf("\t%d", bd_times[i].status.saldo);
+        printf("\t%d\n", bd_times[i].status.pts_ganho);
+    }
+    printf("\n\n\n");
+}
+
+void exibir_menu(const int qtd_times, const int qtd_partidas, Times *bd_times, Partidas *bd_partidas)
 {
     char escolha;
     while(escolha != 'Q' && escolha != 'q')
@@ -20,10 +46,10 @@ void exibir_menu()
     switch(escolha)
     {
         //verificar depois se a lista está vazia pela qtd_times e qtd_partidas
+        //se estiver, mostre um aviso em vez de executar a função
         case '1': //consultar_time();
         case '2': //consultar_partidas();
-        case '6': //mostrar_classificação();
-        
+        case '6': mostrar_classificação(qtd_times, bd_times, bd_partidas); break;
         case 'Q': printf("Você escolheu sair do sistema"); break;
         case 'q': printf("Você escolheu sair do sistema"); break;
         default: printf("Não existe essa opção\n");
