6d4122e18c6d10538dddca808ab4d5878d23b28b
RhennerSantos <rhennersantos2005@gmail.com>
2025-11-07 14:43:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,28 +2,27 @@
 #include "menu.h"
 #include <stdio.h>
 
-
 int main()
 {
     //Obtém os arquivos csv
-    char *arq_times = "times.csv";
-    char *arq_partidas = "partidas_completo.csv";
+    char *arq_times = "times.txt";
+    char *arq_partidas = "partidas_completo.txt";
 
     //Retorna um número para inserir no vetor estático e para facilitar a iteração do vetor
-    const int linhas_times = conta_linhas(arq_times);
     const int linhas_partidas = conta_linhas(arq_partidas);
     
     //Cria e preenche o banco de dados de Times e Partidas
-    Times *bd_times = criar_bdT(linhas_times, arq_times);
+    Times *bd_times = criar_bdT(arq_times);
     Partidas *bd_partidas = criar_bdP(linhas_partidas, arq_partidas);
     
-    //Calcula e armazena dados do TAD times que é contido no BDtimes
-    calcula_estatistica(linhas_times, linhas_partidas, bd_times, bd_partidas);
+    // //Calcula e armazena dados do TAD times que é contido no BDtimes
+    calcula_estatistica(linhas_partidas, bd_times, bd_partidas);
     
     //Mostra a tela no terminal
-    exibir_menu(linhas_times, linhas_partidas, bd_times, bd_partidas);
+    exibir_menu(linhas_partidas, bd_times, bd_partidas);
     
     return 0;
 }
+
 
 
