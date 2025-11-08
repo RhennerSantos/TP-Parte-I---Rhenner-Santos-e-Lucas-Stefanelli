4c07eb1cd23467b8cfe7a28849cf07e4d214c79c
RhennerSantos <rhennersantos2005@gmail.com>
2025-11-07 21:59:56 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -9,12 +9,16 @@ void exibir_menu(const int qtd_partidas, Times *bd_times, Partidas *bd_partidas)
 //consulta os times
 void consultar_time(Times *bd_times);
 
+// Consultar partidas (time mandante, time visitante, etc)
+void consultar_partidas(const int qtd_partidas, Times *bd_times, Partidas *bd_partidas);
+
 //mostrar classificação
 void mostrar_classificação(Times *bd_times, Partidas *bd_partidas);
 
 //limpa a tela
 void limpa_tela();
 
+//transforma a string em minúsculo
 void lowercase(char *outro_time, char *nome_time, int tamanho_nome);
 
 #endif
